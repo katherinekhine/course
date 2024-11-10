5b665a911077bca34d24ff841fc0ccbea1b46ffb
katherinekhine <khaingkatherine@gmail.com>
2024-11-10 12:06:10 +0630

    add middleware for Course, Role,Chapter in Controller

--- app/Http/Controllers/ChapterController.php
+++ app/Http/Controllers/ChapterController.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\Auth;
 
 class ChapterController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except('index');
+    }
     /**
      * Display a listing of the resource.
      */
