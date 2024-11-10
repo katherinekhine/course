bad3ef2e84e7935e4ba88570b06c72fa8b59b09c
katherinekhine <khaingkatherine@gmail.com>
2024-11-10 11:41:22 +0630

    update CourseStudentController.php for register and unregister

--- app/Http/Controllers/CourseStudentController.php
+++ app/Http/Controllers/CourseStudentController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CourseStudentController extends Controller
 {
-    //
+    public function register(Course $course)
+    {
+        $course->students()->attach(Auth::id());
+        return redirect(route('courses.index'));
+    }
+
+    public function unregister(Course $course)
+    {
+        $course->students()->detach(Auth::id());
+        return redirect(route('courses.index'));
+    }
 }
